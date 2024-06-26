144dc98d3396d369d996102b9c1e853f21a421f1
Killian Lecrut <killianlecrut@Air-de-Killian.lan>
2024-06-26 17:11:45 +0200

    correctif carrousel

--- script.js
+++ script.js
@@ -23,14 +23,78 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function applyInertia(speed) {
         const inertiaInterval = setInterval(() => {
-            speed *= 0.95; // Diminuer la vitesse de 5% à chaque intervalle
+            speed *= 0.98; // Réduire la vitesse plus lentement
             rotation += speed;
             carousel.style.transform = `rotateY(${rotation}deg)`;
             if (Math.abs(speed) < 0.1) {
                 clearInterval(inertiaInterval);
             }
         }, 20);
+        setTimeout(() => {
+            clearInterval(inertiaInterval);
+        }, 800000); // Durée de l'inertie (par exemple, 8000 ms)
     }
+    
+
+    // carousel.addEventListener('mousedown', (e) => {
+    //     isDragging = true;
+    //     startX = e.pageX - carousel.offsetLeft;
+    //     startTime = Date.now();
+    //     carousel.style.transition = 'none';
+    //     stopAutoRotate();
+    // });
+
+    // document.addEventListener('mouseup', (e) => {
+    //     if (!isDragging) return;
+    //     isDragging = false;
+    //     endTime = Date.now();
+    //     const timeDiff = endTime - startTime;
+    //     const distance = (e.pageX - carousel.offsetLeft) - startX;
+    //     const speed = distance / timeDiff;
+    //     applyInertia(speed * 8000); // Appliquer l'inertie en fonction de la vitesse du mouvement
+    //     carousel.style.transition = 'transform 0.5s linear';
+    //     startAutoRotate();
+    // });
+
+    // document.addEventListener('mousemove', (e) => {
+    //     if (!isDragging) return;
+    //     e.preventDefault();
+    //     currentX = e.pageX - carousel.offsetLeft;
+    //     const walk = (currentX - startX) * 0.6;
+    //     rotation += walk;
+    //     carousel.style.transform = `rotateY(${rotation}deg)`;
+    //     startX = currentX;
+    // });
+
+    // carousel.addEventListener('touchstart', (e) => {
+    //     isDragging = true;
+    //     startX = e.touches[0].pageX - carousel.offsetLeft;
+    //     startTime = Date.now();
+    //     carousel.style.transition = 'none';
+    //     stopAutoRotate();
+    // });
+
+    // document.addEventListener('touchend', (e) => {
+    //     if (!isDragging) return;
+    //     isDragging = false;
+    //     endTime = Date.now();
+    //     const timeDiff = endTime - startTime;
+    //     const distance = (e.changedTouches[0].pageX - carousel.offsetLeft) - startX;
+    //     const speed = distance / timeDiff;
+    //     applyInertia(speed * 8000); // Appliquer l'inertie en fonction de la vitesse du mouvement
+    //     carousel.style.transition = 'transform 0.5s linear';
+    //     startAutoRotate();
+    // });
+
+    // document.addEventListener('touchmove', (e) => {
+    //     if (!isDragging) return;
+    //     currentX = e.touches[0].pageX - carousel.offsetLeft;
+    //     const walk = (currentX - startX) * 0.3;
+    //     rotation += walk;
+    //     carousel.style.transform = `rotateY(${rotation}deg)`;
+    //     startX = currentX;
+    // });
+
 
     carousel.addEventListener('mousedown', (e) => {
         isDragging = true;
@@ -39,7 +103,7 @@ document.addEventListener('DOMContentLoaded', function () {
         carousel.style.transition = 'none';
         stopAutoRotate();
     });
-
+    
     document.addEventListener('mouseup', (e) => {
         if (!isDragging) return;
         isDragging = false;
@@ -47,21 +111,21 @@ document.addEventListener('DOMContentLoaded', function () {
         const timeDiff = endTime - startTime;
         const distance = (e.pageX - carousel.offsetLeft) - startX;
         const speed = distance / timeDiff;
-        applyInertia(speed * 100); // Appliquer l'inertie en fonction de la vitesse du mouvement
-        carousel.style.transition = 'transform 0.5s ease';
+        applyInertia(speed * 8000); // Appliquer l'inertie en fonction de la vitesse du mouvement
+        carousel.style.transition = 'transform 0.5s linear';
         startAutoRotate();
     });
-
+    
     document.addEventListener('mousemove', (e) => {
         if (!isDragging) return;
         e.preventDefault();
         currentX = e.pageX - carousel.offsetLeft;
-        const walk = (currentX - startX) * 0.3;
+        const walk = (currentX - startX) * 0.6;
         rotation += walk;
         carousel.style.transform = `rotateY(${rotation}deg)`;
         startX = currentX;
     });
-
+    
     carousel.addEventListener('touchstart', (e) => {
         isDragging = true;
         startX = e.touches[0].pageX - carousel.offsetLeft;
@@ -69,7 +133,7 @@ document.addEventListener('DOMContentLoaded', function () {
         carousel.style.transition = 'none';
         stopAutoRotate();
     });
-
+    
     document.addEventListener('touchend', (e) => {
         if (!isDragging) return;
         isDragging = false;
@@ -77,11 +141,11 @@ document.addEventListener('DOMContentLoaded', function () {
         const timeDiff = endTime - startTime;
         const distance = (e.changedTouches[0].pageX - carousel.offsetLeft) - startX;
         const speed = distance / timeDiff;
-        applyInertia(speed * 100); // Appliquer l'inertie en fonction de la vitesse du mouvement
-        carousel.style.transition = 'transform 0.5s ease';
+        applyInertia(speed * 8000); // Appliquer l'inertie en fonction de la vitesse du mouvement
+        carousel.style.transition = 'transform 0.5s linear';
         startAutoRotate();
     });
-
+    
     document.addEventListener('touchmove', (e) => {
         if (!isDragging) return;
         currentX = e.touches[0].pageX - carousel.offsetLeft;
@@ -90,7 +154,7 @@ document.addEventListener('DOMContentLoaded', function () {
         carousel.style.transform = `rotateY(${rotation}deg)`;
         startX = currentX;
     });
-
+    
     const btcButton = document.querySelector('.carousel-item[data-type="btc"]');
     const ethButton = document.querySelector('.carousel-item[data-type="eth"]');
     const btcNotification = document.getElementById('btcNotification');
@@ -98,16 +162,19 @@ document.addEventListener('DOMContentLoaded', function () {
     const btcAddress = 'votre-adresse-btc-ici';
     const ethAddress = 'votre-adresse-eth-ici';
 
-    btcButton.addEventListener('click', () => {
+    btcButton.addEventListener('click', (e) => {
+        e.preventDefault();
         navigator.clipboard.writeText(btcAddress).then(() => {
             btcNotification.style.display = 'block';
             setTimeout(() => {
                 btcNotification.style.display = 'none';
             }, 2000);
         });
+        
     });
 
-    ethButton.addEventListener('click', () => {
+    ethButton.addEventListener('click', (e) => {
+        e.preventDefault();
         navigator.clipboard.writeText(ethAddress).then(() => {
             ethNotification.style.display = 'block';
             setTimeout(() => {
